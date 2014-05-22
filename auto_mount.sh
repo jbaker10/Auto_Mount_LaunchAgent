19d718416abc6a2523bdb16e54f1bf199b0e87ee
jbaker10 <jer.baker10@gmail.com>
2014-05-22 10:56:10 -0400

    Added edited date

--- auto_mount.sh
+++ auto_mount.sh
@@ -4,7 +4,7 @@
 #  
 #
 #  Created by Jeremiah Baker on 5/14/14.
-#
+#  Modified 5/22/14
 
 ##########################################################
 ## This script is intended for Macs bound to an Active Directory with Network accounts
